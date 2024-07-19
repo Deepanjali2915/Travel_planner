0a24bf335b41dd4713cd9bff4ddb3257a946a5d1
poojarani1234 <ranipooja21@navgurukul.org>
2024-07-19 11:12:10 +0530

    filter-data done

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,23 +1,17 @@
-// components/Navbar.js
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <>
     <div className="navbar">
-        {/* <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgoM2I3Q6Zb_okRag_dFWFhNnRtwDoBxKX2A&s" /> */}
-        <a href="https://www.recipetineats.com/" className="active">Home</a>
+      <h1>Trvler Platform</h1>
+      <div className="nav-links">
+        <a href="/" className="active">Home</a>
         <a href="/destination">Destination</a>
-        <a href="Recipes">Hotel </a>
-        <input className="input" type="text" placeholder="Enter a country"/>
-        {/* <img className="icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJlJNDOU-G_CL6NQv_fDCgps3Mqq1QFXPRK0rXyRAXf2_u_hyZ6UPb45zGvGt1BbT1fic&usqp=CAU"/> */}
+        <a className="hotel" href="Recipes">Hotel</a>
         <a className="login" href="#Login">Login</a>
-        <a  className="sign" href="#Sign up">Sign-up</a>
-        <a  className="sign" href="#Sign up">Book Now</a>
-
+        <a className="sign" href="#Sign up">Sign-up</a>
+      </div>
     </div>
-   
-    </>
   );
 };
 
